3bd244d366a958117c482f2185d617397210cd67
Samuel <belloalonsosamuel@gmail.com>
2021-05-12 12:26:49 +0200

    borrado

--- .flaskenv
+++
@@ -1,2 +0,0 @@
-FLASK_APP=app/geoportal.py
-FLASK_ENV=development
--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-
-class Config(object):
-    SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:root@localhost:5432/Geoportal_BD"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
